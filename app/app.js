93e206f970fddc96a501cbf03adf0b6b79ba1b8f
Maggie Oudsema <moudsema@itsdelivers.com>
2021-08-05 13:29:53 -0400

    double binding for image

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
+
+(function () {
+
+  var app = angular.module("gitHubViewer", []);
 
 
+  var mainController = function ($scope,) {
+    var person = {
+      firstName: "Maggie",
+      lastName: "Oudsema",
+      imageSrc: "https://pbs.twimg.com/profile_images/1336092056243101700/MwrUZoe3_400x400.jpg"
+    }
+    $scope.message = "hello Angular!";
+    $scope.person = person
+  };
+
+  app.controller("mainController", mainController);
+})();
