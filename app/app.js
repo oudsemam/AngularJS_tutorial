ba897a894c873e05cb381123623e92f82a4d9300
Maggie Oudsema <moudsema@itsdelivers.com>
2021-08-05 13:38:59 -0400

    put an error and bound it to html if somethign has gone wrong

--- app/app.js
+++ app/app.js
@@ -10,10 +10,14 @@
     var onUserComplete =function(response) {
       $scope.user = response.data;
     };
+
+    var onError = function(reason){
+      $scope.error = "Could not fetch the user.";
+    }
     
     $http.get("https://api.github.com/users/robconery")
 
-    .then(onUserComplete);
+    .then(onUserComplete, onError);
 
     $scope.message = "hello Angular!";
     
